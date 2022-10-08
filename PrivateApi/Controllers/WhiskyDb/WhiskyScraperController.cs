9a887a5450f21dc9460ab7e0564a5ceacee6544c
Maximilian Ditz <max.ditz@outlook.de>
2022-10-08 14:32:40 +0200

    initial commit

---
+++ PrivateApi/Controllers/WhiskyDb/WhiskyScraperController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PrivateApi.Controllers.WhiskyDb
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class WhiskyScraperController : ControllerBase
+    {
+    }
+}
